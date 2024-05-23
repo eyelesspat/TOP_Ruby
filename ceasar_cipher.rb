be1aae3e240b2799083d9879b5108aa61ad8dfab
Patryk Gadziński <pgadzinski@cross-net.eu>
2024-05-23 08:21:53 +0200

    Update code to be shorter

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,29 +1,14 @@
 def caesar_cipher(string, shift_factor)
-  array = string.split('')
-
-  array.map!(&:ord)
-
-  array.map! do |number|
-    if number >= 65 && number <= 90
-      if number + shift_factor <= 90
-        number + shift_factor
-      else
-        number - 26 + shift_factor
-      end
-
-    elsif number >= 97 && number <= 122
-      if number + shift_factor <= 122
-        number + shift_factor
-      else
-        number - 26 + shift_factor
-      end
+  array = string.split('').map!(&:ord).map! do |number|
+    if number.between?(65, 90)
+      number + shift_factor <= 90 ? (number + shift_factor) : number - 26 + shift_factor
+    elsif number.between?(97, 122)
+      number + shift_factor <= 122 ? (number + shift_factor) : number - 26 + shift_factor
     else
       number
     end
   end
-  final_array = array.map(&:chr)
-  final_array = final_array.join
-  puts final_array
+  puts array.map(&:chr).join
 end
 
 caesar_cipher('What a string!', 5)
